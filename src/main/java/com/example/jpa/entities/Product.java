ce0ba6a85eba974f03da65b825ec789474a3871f
victor <victor.sismotto@gmail.com>
2024-03-17 18:38:27 -0300

    payment part

--- src/main/java/com/example/jpa/entities/Product.java
+++ src/main/java/com/example/jpa/entities/Product.java
@@ -1,30 +1,19 @@
 package com.example.jpa.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
 
+import java.io.Serial;
 import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
-import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
 @NoArgsConstructor
 @Table(name = "tb_product")
 public class Product implements Serializable {
+    @Serial
     private static final long serialVersionUID = 1L;
 
     @Id
